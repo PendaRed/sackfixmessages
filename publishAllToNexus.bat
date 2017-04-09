44f20aae40389511b4e7b4c9c839731f48d3977d
Jonathan <gibbons_jonathan@hotmail.com>
2017-04-09 14:53:06 +0100

    Published to nexus

---
+++ publishAllToNexus.bat
@@ -0,0 +1,31 @@
+REM stupid SBT runs out of mem on my dev lappie
+REM sbt sackfixmessagesfixt11/compile
+REM sbt sackfixmessagesfixt11/publish-local
+REM
+REM sbt is a .bat, so stick call infront of it
+set JAVA_OPTS=-Xmx1024M -Xss2M
+
+REM This takes about an hour to run!
+call sbt "project sackfixmessages40" publishSigned
+
+call sbt "project sackfixmessages41" publishSigned
+
+call sbt "project sackfixmessages42" publishSigned
+
+call sbt "project sackfixmessages43" publishSigned
+
+call sbt "project sackfixmessages44" publishSigned
+
+call sbt "project sackfixmessages44modified" publishSigned
+
+call sbt "project sackfixmessagesfixt11" publishSigned
+
+call sbt "project sackfixmessages50" publishSigned
+
+call sbt "project sackfixmessages50sp1" publishSigned
+
+call sbt "project sackfixmessages50sp1modified" publishSigned
+
+call sbt "project sackfixmessages50sp2" publishSigned
+
+call sbt "project sackfixmessages50sp2modified" publishSigned
