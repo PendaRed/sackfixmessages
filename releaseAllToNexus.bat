1b00deaf207a5e8dcaf52bbac4d518b222422226
Jonathan Gibbons <gibbons_jonathan@hotmail.com>
2021-04-07 15:29:29 +0100

    Updated the readme

--- releaseAllToNexus.bat
+++ releaseAllToNexus.bat
@@ -5,26 +5,26 @@ REM
 REM sbt is a .bat, so stick call infront of it
 set JAVA_OPTS=-Xmx1024M -Xss2M
 
-call sbt "project sackfixmessages40" publishSigned
+call sbt "project sackfixmessages40" sonatypeRelease
 
-call sbt "project sackfixmessages41" publishSigned
+call sbt "project sackfixmessages41" sonatypeRelease
 
-call sbt "project sackfixmessages42" publishSigned
+call sbt "project sackfixmessages42" sonatypeRelease
 
-call sbt "project sackfixmessages43" publishSigned
+call sbt "project sackfixmessages43" sonatypeRelease
 
-call sbt "project sackfixmessages44" publishSigned
+call sbt "project sackfixmessages44" sonatypeRelease
 
-call sbt "project sackfixmessages44modified" publishSigned
+call sbt "project sackfixmessages44modified" sonatypeRelease
 
-call sbt "project sackfixmessagesfixt11" publishSigned
+call sbt "project sackfixmessagesfixt11" sonatypeRelease
 
-call sbt "project sackfixmessages50" publishSigned
+call sbt "project sackfixmessages50" sonatypeRelease
 
-call sbt "project sackfixmessages50sp1" publishSigned
+call sbt "project sackfixmessages50sp1" sonatypeRelease
 
-call sbt "project sackfixmessages50sp1modified" publishSigned
+call sbt "project sackfixmessages50sp1modified" sonatypeRelease
 
-call sbt "project sackfixmessages50sp2" publishSigned
+call sbt "project sackfixmessages50sp2" sonatypeRelease
 
-call sbt "project sackfixmessages50sp2modified" publishSigned
+call sbt "project sackfixmessages50sp2modified" sonatypeRelease
